31dfd52a5af4f4f562d8e482c860b0261c2ea369
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-05-04 09:28:52 +0100

    Feature: Delete notes

--- src/validations/note.validator.js
+++ src/validations/note.validator.js
@@ -34,6 +34,19 @@ export default class NotesValidator {
     ]
   }
 
+  static validateNotesDeletionData () {
+    return [
+      check('noteIds')
+        .exists()
+        .withMessage('Note ids is required')
+        .not()
+        .isString()
+        .withMessage('Note ids must be a nonstring array')
+        .isArray({ min: 1 })
+        .withMessage('Note ids must be an unempty array')
+    ]
+  }
+
   /**
    * @returns {JSON} JSON error object if req contains error
    * @returns {next} - passes control to next function if req contains no error
