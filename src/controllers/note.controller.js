2df4b7f4a58548679101e4415b0afc906dea9d02
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-05-04 06:59:38 +0100

    Feature: Fetch notes

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -54,7 +54,7 @@ export default class NotesController {
 
       Response.Success(res, { notes })
     } catch (err) {
-      Response.InternalServerError(res, 'Error fetching note')
+      Response.InternalServerError(res, 'Error fetching notes')
     }
   }
 }
