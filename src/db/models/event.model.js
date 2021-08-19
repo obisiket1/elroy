f32f6a92f2ea16c2f1b018ce9decb24acde141bc
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-08-19 21:58:11 +0100

    Chore: Edit event model

--- src/db/models/event.model.js
+++ src/db/models/event.model.js
@@ -28,6 +28,9 @@ const eventSchema = new mongoose.Schema(
     requirePassword: Boolean,
     displayImage: String,
     isLive: Boolean,
+    likesCount: { type: Number, default: 0 },
+    viewsCount: { type: Number, default: 0 },
+    reviewsCount: { type: Number, default: 0 }
   },
   {
     timestamps: true,
@@ -58,3 +61,36 @@ eventSchema.index({ location: '2dsphere' })
 
 const Event = mongoose.model('event', eventSchema)
 export default Event
+
+import mongoose from 'mongoose'
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  },
+  description: String,
+  location: Object,
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'category'
+  },
+  startDate: Date,
+  endDate: Date,
+  attendanceLimit: Number,
+  requirePassword: Boolean,
+  password: String,
+  requireAuthentication: Boolean,
+  backgroundImage: String,
+  likesCount: { type: Number, default: 0 },
+  viewsCount: { type: Number, default: 0 },
+  reviewsCount: { type: Number, default: 0 }
+})
+
+const Event = mongoose.model('event', eventSchema)
+export default Event
+
